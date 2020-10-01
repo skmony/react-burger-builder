fe21c55f83297084c92aa3033eb0de0fba7f945c
Shashikant Mony <shashikantmony@gmail.com>
2020-10-01 17:39:22 +0530

    Add Signin and Signup Reducer

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,41 @@
+import * as actionTypes from "../actions/actionTypes";
+import { updateObj } from "../utility";
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false,
+};
+
+const authStart = (state, action) => {
+  return updateObj(state, { error: null, loading: true });
+};
+
+const authSuccess = (state, action) => {
+  return updateObj(state, {
+    error: null,
+    loading: false,
+    token: action.idToken,
+    userId: action.userId,
+  });
+};
+const authFail = (state, action) => {
+  return updateObj(state, { error: action.error, loading: false });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
