bd3b5451b887613d8652a123c00d87664d94e26d
Shashikant Mony <shashikantmony@gmail.com>
2020-10-05 01:23:08 +0530

    Change UI state with change in Auth

---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+
+import * as actionTypes from "../../../store/actions/";
+
+class Logout extends Component {
+  componentDidMount() {
+    console.log("HIT");
+    this.props.onLogout();
+  }
+  render() {
+    return <Redirect to="/" />;
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogout: () => dispatch(actionTypes.logout()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Logout);
