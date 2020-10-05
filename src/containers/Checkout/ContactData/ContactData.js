12171e45100c25562b02dc0d9ff509d35642612c
Shashikant Mony <shashikantmony@gmail.com>
2020-10-05 16:04:20 +0530

    Filter order according to userId

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -141,6 +141,7 @@ class ContactData extends Component {
       ingredients: this.props.ingredients,
       price: this.props.price,
       orderData: formData,
+      userId: this.props.userId,
     };
     this.props.onOrderBurger(order, this.props.token);
   };
@@ -219,6 +220,7 @@ const mapStateToProps = (state) => {
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -11,7 +11,7 @@ import Spinner from "../../components/UI/Spinners/Spinners";
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders(this.props.token);
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
   render() {
     let orders = <Spinner />;
@@ -33,12 +33,14 @@ const mapStateToProps = (state) => {
     order: state.order.order,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onFetchOrders: (token) => dispatch(actionTypes.fetchOrders(token)),
+    onFetchOrders: (token, userId) =>
+      dispatch(actionTypes.fetchOrders(token, userId)),
   };
 };
 
