608ceb1641a56a50010a6df2d6d6dae9a141114c
Shashikant Mony <shashikantmony@gmail.com>
2020-10-05 17:14:21 +0530

    Export check validity

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import { Redirect } from "react-router-dom";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import Spinners from "../../components/UI/Spinners/Spinners";
+import { checkValidity } from "../../shared/checkvalidity";
 
 import classes from "./Auth.css";
 
@@ -48,30 +49,6 @@ class Auth extends Component {
     isSignup: true,
   };
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    if (rules.isEmail) {
-      const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    if (rules.isNumeric) {
-      const pattern = /^[0-9]+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    return isValid;
-  };
   componentDidMount() {
     if (!this.props.building && this.props.authRedirect !== "/") {
       this.props.onSetAuthRedirectPath();
@@ -83,7 +60,7 @@ class Auth extends Component {
       [controlName]: {
         ...this.state.controls[controlName],
         value: event.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           event.target.value,
           this.state.controls[controlName].validation
         ),
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import withErrorHandler from "../../../hoc/WithErrorHandler/WithErrorHandler";
 import Input from "../../../components/UI/Input/Input";
 import { connect } from "react-redux";
 import * as actionTypes from "../../../store/actions/";
+import { checkValidity } from "../../../shared/checkvalidity";
 
 class ContactData extends Component {
   state = {
@@ -102,31 +103,6 @@ class ContactData extends Component {
     formIsValid: false,
   };
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    if (rules.isEmail) {
-      const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    if (rules.isNumeric) {
-      const pattern = /^[0-9]+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    return isValid;
-  };
-
   orderHandler = (event) => {
     event.preventDefault();
 
@@ -154,7 +130,7 @@ class ContactData extends Component {
 
     updatedFormField.value = event.target.value;
     updatedFormField.touched = true;
-    updatedFormField.valid = this.checkValidity(
+    updatedFormField.valid = checkValidity(
       updatedFormField.value,
       updatedFormField.validation
     );
